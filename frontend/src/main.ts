6fd6d2f9d47e6e08786267544bdfaf19c16b16dc
Hamza Agar <hamza.agar@d-teknoloji.com.tr>
2023-05-27 21:50:02 +0300

    Environment variables added to Vue project

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import signalR from './signalR'
-
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 app.use(signalR, {
-  baseUrl: 'https://localhost:11000/chat'
+  baseUrl: `${process.env['CONNECTIONSTRINGS__CHAT-SERVER']}/chat`
 })
 app.config.globalProperties.$chatHub.on('user', (user) => {
   window.console.log(user)
